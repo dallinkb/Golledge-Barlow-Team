f1a7a77ddcdaf5a4c6c0ab9153aa320f0e530fe5
Dallin Barlow <Dallin Barlow@LAPTOP-VKQ5VTM2>
2016-02-13 23:41:30 -0700

    2/13/2016 - first team controller package done

---
+++ DesertEscape/src/byui/cit260/desertescape/control/puzzles.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.desertescape.control;
+
+/**
+ *
+ * @author Dallin Barlow
+ */
+public class puzzles {
+    
+    public double treeCuttingCalculator(double diameter, double height, double strokes) { 
+        
+        if(diameter <= 0 || height <= 0 || strokes <= 0) {
+            return -1;
+        }
+        
+        double total = diameter * height/strokes;
+        
+        return total;
+        
+    }          
+}
---
+++ DesertEscape/test/byui/cit260/desertescape/control/puzzlesTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.desertescape.control;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dallin Barlow
+ */
+public class puzzlesTest {
+    
+    public puzzlesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+    /**
+     * Test of treeCuttingCalculator method, of class puzzles.
+     */
+    @Test
+    public void testTreeCuttingCalculator() {
+        System.out.println("treeCuttingCalculator");
+        double diameter = 5.0;
+        double height = 6.0;
+        double strokes = 3.0;
+        puzzles instance = new puzzles();
+        double expResult = 10.0;
+        double result = instance.treeCuttingCalculator(diameter, height, strokes);
+        assertEquals(expResult, result, 0.01);
+    }
+    
+}
